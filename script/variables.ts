fc78011b796fa29e2af886d110e10f55dfa0d75a
Aurélien <a.perraudussel@gmail.com>
2017-10-30 13:24:56 +0100

    Add colors and typings

--- script/variables.ts
+++ script/variables.ts
@@ -1,12 +1,12 @@
 let Variables = {
     turn: 0,
-    colors: ['#ff0000', 'green', 'blue', 'orange', 'purple'],
-    solution: [],
-    start: true,
-    playing:  false,
-    end: false,
-    entree: [],
-    lignes: 10,
-    colonnes: 4
+    colors: <Array<string>> ['#ff0000', 'green', 'blue', 'orange', 'purple', '#666', '#ff89be'],
+    solution: <Array<number>> [],
+    start: <boolean> true,
+    playing:  <boolean> false,
+    end: <boolean> false,
+    entree: <Array<number>> [],
+    lignes: <number> 10,
+    colonnes: <number> 4
 }
 export default Variables;
